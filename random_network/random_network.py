fb9d8b9c747d32acb270ee4b5bc9f977167337ac
Abhipray Sahoo <abhiprays@gmail.com>
2019-10-03 00:03:59 -0700

    encapsulate in class

--- random_network/random_network.py
+++ random_network/random_network.py
@@ -12,36 +12,76 @@ import random
 import numpy as np
 from hebbian_lms import HebbLMSNet
 
-n_input = 4
-n_output = 4
-n_hidden = 5
-N = n_output+n_hidden+n_input
-p = 0.1
-mu = 0.05
-gamma = 0.5
+
+
 n_iters = 100
 
-d = nx.DiGraph()
 
-# Add input nodes
-for i in range(n_input):
-    d.add_node(i)
+class RatsNest:
+    def __init__(self, n_input, n_output, n_hidden, p=0.3, mu=0.1, gamma=0.5):
+        print('d')
+        d = nx.DiGraph()
 
-# Add new nodes sequentially and connect to other random nodes
-for i in range(n_input, N):
-    # print(i)
-    indegree = 0
-    d.add_node(i)
-    for candidate in d.nodes:
-        if random.random() > p and candidate != i:
-            print(i, candidate)
-            d.add_edge(candidate, i)
-            indegree += 1
-    # Add a hebblms neuron to this node
-    hebb = HebbLMSNet(indegree, [1], 0.5, mu=mu, gamma=gamma)
-    d.nodes[i]['net'] = hebb
-    d.nodes[i]['output'] = 0
+        N = n_output + n_hidden + n_input # Total number of nodes
 
+        # Add input nodes
+        for i in range(n_input):
+            d.add_node(i)
+
+        # Add new nodes sequentially and connect to other random nodes
+        for i in range(n_input, N):
+            # print(i)
+            indegree = 0
+            d.add_node(i)
+            for candidate in d.nodes:
+                if random.random() > p and candidate != i:
+                    d.add_edge(candidate, i)
+                    indegree += 1
+            # Add a hebblms neuron to this node
+            hebb = HebbLMSNet(indegree, [1], 0.5, mu=mu, gamma=gamma)
+            d.nodes[i]['net'] = hebb
+            d.nodes[i]['output'] = 0
+
+        self.net = d
+        self.n_input = n_input
+        self.n_output = n_output
+        self.n_hidden = n_hidden
+        self.N = N
+
+    def get_graph(self):
+        return self.net
+
+    def run(self, X):
+        errors = []
+        d = self.net
+
+        for i in range(n_training_samples):
+            # Load the input
+            for k in range(self.n_input):
+                d.nodes[k]['output'] = X[i][k]
+            err_sum = 0
+            # Train each neuron sequentially
+            for k in range(self.n_input, self.N):
+                hebb = d.nodes[k]['net']
+                # Construct the input vector from its parents
+                x = []
+                parents = d.predecessors(k)
+                for p in parents:
+                    x.append(d.nodes[p]['output'])
+                x = np.array(x)[None, :]
+                y, _, err = hebb.run(x)
+                err_sum += err[0][0] ** 2
+                d.nodes[k]['output'] = y[0][0]
+            errors.append(err_sum / (self.N - self.n_input))
+
+
+n_input=4
+n_output=4
+n_hidden=10
+N = n_hidden+n_input+n_output
+
+rat = RatsNest(n_input, n_output, n_hidden)
+d = rat.get_graph()
 
 pos=nx.shell_layout(d)
 
@@ -66,27 +106,8 @@ plt.show()
 n_training_samples = 50
 X = np.random.rand(n_training_samples, n_input)
 
-errors = []
-for epoch in range(n_iters):
-    for i in range(n_training_samples):
-        # Load the input
-        for k in range(n_input):
-            d.nodes[k]['output'] = X[i][k]
-        err_sum = 0
-        # Train each neuron sequentially
-        for k in range(n_input, N):
-            hebb = d.nodes[k]['net']
-            # Construct the input vector from its parents
-            x = []
-            parents = d.predecessors(k)
-            for p in parents:
-                x.append(d.nodes[p]['output'])
-            x = np.array(x)[None, :]
-            y,_,err = hebb.run(x)
-            err_sum += err[0][0]**2
-            d.nodes[k]['output'] = y[0][0]
-        errors.append(err_sum/(N-n_input))
+rat.run(X)
 
 plt.figure()
-plt.plot(errors)
+# plt.plot(errors)
 plt.show()
